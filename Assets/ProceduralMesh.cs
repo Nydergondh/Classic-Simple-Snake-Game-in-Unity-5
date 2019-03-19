19ab211c75fc1a13cd6e2cec514bfcd0728199e2
Vinicius Rosa <viniciusr25@hotmail.com>
2019-03-19 20:15:13 -0300

    Update ProceduralMesh.cs

--- Assets/ProceduralMesh.cs
+++ Assets/ProceduralMesh.cs
@@ -20,8 +20,6 @@ public class ProceduralMesh : MonoBehaviour
     [SerializeField] float cellSize;
     private float vertexOffset;
     
-    
-
     // Start is called before the first frame update
     void Start()
     {
